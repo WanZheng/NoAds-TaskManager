747781879acbf9cf926307403df136b2fac2e7f9
Wilson Wan <wanzheng@minigui.org>
2011-09-19 14:33:24 +0800

    add function onResume()

--- src/me/cos/taskmanager/TaskKiller.java
+++ src/me/cos/taskmanager/TaskKiller.java
@@ -4,8 +4,13 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class TaskKiller extends Activity {
-    @Override public void onCreate(Bundle savedInstanceState) {
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.task_killer);
+    }
+
+    @Override protected void onResume() {
+	super.onResume();
     }
 }
