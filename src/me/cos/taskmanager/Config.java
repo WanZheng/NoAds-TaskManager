b2d2fc425092c9a0b5775ce38d634b572af04c50
Wilson Wan <wanzheng@minigui.org>
2011-09-19 15:44:20 +0800

    implement a simple task killer, which can support multi-kill

---
+++ src/me/cos/taskmanager/Config.java
@@ -0,0 +1,5 @@
+package me.cos.taskmanager;
+
+public class Config {
+    public static final String TAG = "TaskManager";
+}
--- src/me/cos/taskmanager/TaskKiller.java
+++ src/me/cos/taskmanager/TaskKiller.java
@@ -1,16 +1,88 @@
 package me.cos.taskmanager;
 
+import java.util.List;
+import java.util.Iterator;
+
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningAppProcessInfo;
+import android.util.Log;
+import android.util.SparseBooleanArray;
 
 public class TaskKiller extends Activity {
+    private ActivityManager mActivityManager;
+    private ListView mListView;
+    private MyListAdapter mAdapter;
 
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.task_killer);
+
+	mAdapter = new MyListAdapter(this);
+
+	mListView = (ListView) findViewById(R.id.list);
+	mListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+	// XXX: mListView.setItemsCanFocus(false);
+	mListView.setAdapter(mAdapter);
     }
 
     @Override protected void onResume() {
 	super.onResume();
+
+	mActivityManager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+	refresh();
+    }
+
+    private void refresh() {
+	mAdapter.clear();
+
+	List<RunningAppProcessInfo> procList = mActivityManager.getRunningAppProcesses();
+        
+    	for (Iterator<RunningAppProcessInfo> iterator = procList.iterator(); iterator.hasNext();) {
+    		RunningAppProcessInfo procInfo = iterator.next();
+    		mAdapter.add(procInfo.processName);
+    	}
+
+	mAdapter.notifyDataSetChanged();
+	// TODO:
+	mListView.clearChoices();
+    }
+
+    public void onKill(View view) {
+	SparseBooleanArray checkedPositions = mListView.getCheckedItemPositions();
+	for (int i=0; i<checkedPositions.size(); i++) {
+	    if (checkedPositions.valueAt(i)) {
+		String processName = (String) mAdapter.getItem(checkedPositions.keyAt(i));
+		Log.d(Config.TAG, "kill " + processName);
+		mActivityManager.killBackgroundProcesses(processName);
+	    }
+	}
+    	
+    	refresh();
+    }
+
+    private class MyListAdapter extends ArrayAdapter<String> {
+	public MyListAdapter(Context context) {
+	    super(context, android.R.layout.simple_list_item_multiple_choice);
+	}
+
+	// @Override public View getView(int position, View convertView, ViewGroup parent) {
+	//     String packageName = (String) getItem(position);
+	//     TextView view = (TextView) convertView;
+
+	//     if (view == null) {
+	// 	view = new TextView(TaskKiller.this);
+	//     }
+	//     view.setText(packageName);
+
+	//     return view;
+	// }
     }
 }
